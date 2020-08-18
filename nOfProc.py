80c46f89a4192110cad44c977361a977b81d7a78
CarloPa <54579322+Carlopasquinucci@users.noreply.github.com>
2020-08-18 16:06:57 +0200

    Create nOfProc.py

---
+++ nOfProc.py
@@ -0,0 +1,19 @@
+# version 1.0
+# created by Carlo Pasquinucci - carlo.a.pasquinucci@gmail.com
+# relesead under license GPL GNU 3
+
+import sys
+
+narg=len(sys.argv)
+if narg!=2:
+    print("Number of argument is not 1")
+    if narg>2:
+        print("Reduce the number of arguments")
+    else:
+        print("Increase the number of arguments")
+    exit()
+ cellMesh=float(sys.argv[1])
+ nCellsProProcessor=500000
+ 
+nProcessorAux=nCellsProProcessor/cellMesh
+nProcessor=int(nProcessorAux)
